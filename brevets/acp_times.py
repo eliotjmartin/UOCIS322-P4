41d2affef810348cfd2a18978a666b36204bef10
eliotjmartin <eliotm@uoregon.edu>
2021-05-11 11:40:13 -0700

    accounts for rounding errors + tests added

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -27,19 +27,28 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        This will be in the same time zone as the brevet start time.
     """
     times = {'200': 34, '300': 32, '400': 30, '600': 28, '1000': 26}
-    maxSpeed = times['200']
     if control_dist_km < 200:
-       maxSpeed = times['200']
-    if control_dist_km >= 200:
-       maxSpeed = times['300']
-    if control_dist_km >= 300:
-       maxSpeed = times['400']
-    if control_dist_km >= 400:
-       maxSpeed = times['600']
-    if control_dist_km >= 600:
-       maxSpeed = times['1000']
-    hours = control_dist_km / maxSpeed
-    return brevet_start_time.shift(hours=hours)
+       hours = control_dist_km / times['200']
+    elif control_dist_km >= 200 and control_dist_km < 300:
+       hours = 200 / times['200'] 
+       hours = hours + (control_dist_km - 200) / times['300']
+    elif control_dist_km >= 300 and control_dist_km < 400:
+       hours = 200 / times['200']
+       hours = hours + 100 / times['300'] 
+       hours = hours + (control_dist_km - 300) / times['400']       
+    elif control_dist_km >= 400 and control_dist_km < 600:
+       hours = 200 / times['200']
+       hours = hours + 100 / times['300'] 
+       hours = hours + 100 / times['400']
+       hours = hours + (control_dist_km - 400) / times['600'] 
+    else:
+       hours = 200 / times['200']
+       hours = hours + 100 / times['300'] 
+       hours = hours + 100 / times['400']
+       hours = hours + 200 / times['600']
+       hours = hours + (control_dist_km - 600) / times['1000']
+    minutes = round(hours * 60)
+    return brevet_start_time.shift(minutes=minutes)
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -55,24 +64,33 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        This will be in the same time zone as the brevet start time.
     """
     times = {'200': 15, '300': 15, '400': 15, '600': 11.428, '1000': 13.333}
-    minSpeed = times[str(brevet_dist_km)]
     if control_dist_km < 200:
-       minSpeed = times['200']
-    if control_dist_km >= 200:
-       minSpeed = times['300']
-    if control_dist_km >= 300:
-       minSpeed = times['400']
-    if control_dist_km >= 400:
-       minSpeed = times['600']
-    if control_dist_km >= 600:
-       minSpeed = times['1000']
-    hours = control_dist_km / minSpeed
+       hours = control_dist_km / times['200']
+    elif control_dist_km >= 200 and control_dist_km < 300:
+       hours = 200 / times['200'] 
+       hours = hours + (control_dist_km - 200) / times['300']
+    elif control_dist_km >= 300 and control_dist_km < 400:
+       hours = 200 / times['200']
+       hours = hours + 100 / times['300'] 
+       hours = hours + (control_dist_km - 300) / times['400']       
+    elif control_dist_km >= 400 and control_dist_km < 600:
+       hours = 200 / times['200']
+       hours = hours + 100 / times['300'] 
+       hours = hours + 100 / times['400']
+       hours = hours + (control_dist_km - 400) / times['600'] 
+    else:
+       hours = 200 / times['200']
+       hours = hours + 100 / times['300'] 
+       hours = hours + 100 / times['400']
+       hours = hours + 200 / times['600']
+       hours = hours + (control_dist_km - 600) / times['1000'] 
     if control_dist_km < 60:
        hours = hours + (60 - control_dist_km)/60
-    return brevet_start_time.shift(hours=hours)
+    minutes = round(hours*60)
+    return brevet_start_time.shift(minutes = minutes)
 
 
 
-#t = arrow.get('2020-01-01T12:00:00')
-#a = close_time(15, 200, t).isoformat().format('YYYY-MM-DDTHH:mm')
-#print(a)
+t = arrow.get('2020-01-01T00:00:00')
+a = close_time(200, 400, t).isoformat().format('YYYY-MM-DDTHH:mm')
+print(a)
